67e1f2e1e26150dc3a0cc1e283de868a6433f2f5
ahkui <ahkui@outlook.com>
2019-09-29 16:55:21 +0800

    update

--- install/openpose.sh
+++ install/openpose.sh
@@ -31,8 +31,6 @@ else
     apt install -y --no-install-recommends libcaffe-cpu-dev
 fi
 
-
-
 cd /opt
 
 wget https://github.com/Kitware/CMake/releases/download/v3.14.2/cmake-3.14.2-Linux-x86_64.tar.gz
