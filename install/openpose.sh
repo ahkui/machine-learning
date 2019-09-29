118fa1d22280da2800b7c1321cc5bc83bd6003ec
ahkui <ahkui@outlook.com>
2019-09-29 16:47:58 +0800

    update

--- install/openpose.sh
+++ install/openpose.sh
@@ -40,13 +40,14 @@ tar xzf cmake-3.14.2-Linux-x86_64.tar.gz -C /opt
 rm cmake-3.14.2-Linux-x86_64.tar.gz
 
 export PATH="/opt/cmake-3.14.2-Linux-x86_64/bin:${PATH}"
+export PYTHONPATH=/usr/local/python:$PYTHONPATH
+echo 'export PYTHONPATH=/usr/local/python:$PYTHONPATH' >> /etc/profile
 
 git clone --depth=1 https://github.com/CMU-Perceptual-Computing-Lab/openpose.git
 cd openpose
 git submodule update --init --recursive
 cd models
 sed -i "s,http://posefs1.perception.cs.cmu.edu/OpenPose/models/,$OPENPOSE_MODELS_PROVIDER,g" getModels.sh
-./getModels.sh
 cd ..
 mkdir release
 
@@ -58,17 +59,13 @@ then
           -D USE_NCCL=ON \
           -D BUILD_PYTHON=ON .. \
           -D BUILD_EXAMPLES=OFF \
-          -D BUILD_TESTS=OFF \
-          -D CMAKE_BUILD_TYPE=RELEASE \
-          -D CMAKE_BUILD_SHARED_LIBS=1
+          -D CMAKE_BUILD_TYPE=RELEASE
 else
-    cmake -D BUILD_PYTHON=ON -D GPU_MODE=CPU_ONLY -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_BUILD_SHARED_LIBS=1 .. || true
+    cmake -D BUILD_PYTHON=ON -D GPU_MODE=CPU_ONLY -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE .. || true
     sed -i "362i }" ../3rdparty/caffe/src/caffe/layers/mkldnn_inner_product_layer.cpp
     sed -i "358i {" ../3rdparty/caffe/src/caffe/layers/mkldnn_inner_product_layer.cpp
-    cmake -D BUILD_PYTHON=ON -D GPU_MODE=CPU_ONLY -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_BUILD_SHARED_LIBS=1 ..
+    cmake -D BUILD_PYTHON=ON -D GPU_MODE=CPU_ONLY -D BUILD_EXAMPLES=OFF -D CMAKE_BUILD_TYPE=RELEASE ..
 fi
 
 make -j`nproc`
 make install
-
-rm -rf /opt/openpose
